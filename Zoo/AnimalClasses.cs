834f21895817ed1131cc8932d4eec1d428eb4b90
Michiel Wouters <michiel_woute64@hotmail.com>
2022-02-04 12:11:35 +0100

    Add project files.

---
+++ Zoo/AnimalClasses.cs
@@ -0,0 +1,154 @@
+﻿namespace Zoo
+{
+    // An abstract class can only be inherited from. It cannot be instantiated -> Opposite of sealed
+    public abstract class Animal
+    {
+        // A private variable can only be accessed in this class
+        private string _name;
+
+        private static int _counter;
+
+        public int Id { get; set; }
+
+        public int Age { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(Name));
+                }
+
+                _name = value;
+            }
+        }
+
+        // A Protected Property or Method can only be accessed in this class or derived classes
+        protected Animal(string name)
+        {
+            Id = ++_counter;
+            Name = name;
+        }
+
+        // A virtual method can be overridden or expanded upon in derived classes
+        public virtual string GetAnimalCall()
+        {
+            return "Animal noise goes here";
+        }
+    }
+
+    public abstract class Carnivore : Animal
+    {
+        public string[] FavouritePrey { get; set; }
+
+        public bool IsDangerousToHumans { get; set; }
+
+        // A class that derives from a class with a constructor with variables must provide those variables (base).
+        protected Carnivore(string name, string[] favouritePrey, bool isDangerousToHumans)
+            : base(name)
+        {
+            FavouritePrey = favouritePrey;
+            IsDangerousToHumans = isDangerousToHumans;
+        }
+
+        // Overriding a method allows us to override all existing functionality
+        public override string GetAnimalCall()
+        {
+            return "This predator can roar fiercely:";
+        }
+    }
+
+    public abstract class Herbivore : Animal
+    {
+        public string[] FavouriteFoods { get; set; }
+
+        protected Herbivore(string name, string[] favouriteFoods)
+            : base(name)
+        {
+            FavouriteFoods = favouriteFoods;
+        }
+
+        // Overriding a method allows us to override all existing functionality
+        public override string GetAnimalCall()
+        {
+            return "This docile herbivore has a lovely call:";
+        }
+    }
+
+    public class Cat : Carnivore
+    {
+        public bool IsHouseCat { get; set; }
+
+        public Cat(string name, string[] favouritePrey, bool isDangerousToHumans, bool isHouseCat)
+            : base(name, favouritePrey, isDangerousToHumans)
+        {
+            IsHouseCat = isHouseCat;
+        }
+
+        // Overriding can also use what is already there and expand upon it.
+        public override string GetAnimalCall()
+        {
+            return $"{base.GetAnimalCall()} MEOW!";
+        }
+    }
+
+    public class Dog : Carnivore
+    {
+        public bool IsGoodBoy { get; set; }
+
+        // Default parameters allow for a default value -> These set a value if none is provided
+        public Dog(string name, string[] favouritePrey, bool isDangerousToHumans, bool isGoodBoy = true)
+            : base(name, favouritePrey, isDangerousToHumans)
+        {
+            IsGoodBoy = isGoodBoy;
+        }
+
+        public void FetchStick()
+        {
+            Console.WriteLine("That stick never had a chance");
+        }
+
+        // Overriding can also use what is already there and expand upon it.
+        public override string GetAnimalCall()
+        {
+            return $"{base.GetAnimalCall()} BARK BARK";
+        }
+    }
+
+    public class Cow : Herbivore
+    {
+        private int _amountOfStomachs;
+
+        public int AmountOfStomachs
+        {
+            get => _amountOfStomachs;
+            set
+            {
+                if (value > 0)
+                {
+                    _amountOfStomachs = value;
+                }
+            }
+        }
+
+        public Cow(string name, string[] favouriteFoods, int amountOfStomachs = 4)
+            : base(name, favouriteFoods)
+        {
+            AmountOfStomachs = amountOfStomachs;
+        }
+
+        public void ProduceMilk()
+        {
+            Console.WriteLine("Milk for the milk gods!");
+        }
+
+        // Overriding can also use what is already there and expand upon it.
+        public override string GetAnimalCall()
+        {
+            return $"{base.GetAnimalCall()} MOOOOOO!";
+        }
+    }
+}
---
+++ Zoo/Program.cs
@@ -0,0 +1,98 @@
+﻿using Zoo;
+
+Console.WriteLine("Hello, World!");
+
+IList<Animal> animals = GenerateAnimals();
+PrintAnimals(animals);
+
+IList<Animal> GenerateAnimals()
+{
+    // Polymorphism -> Derived classes can take the shape of their base class
+    // Constructor values MUST be provided. Public Properties are optional
+    Animal cat = new Cat("Felix", new[] { "birds", "mice" }, false, true) { Age = 2 };
+    Animal dog = new Dog("Max", new[] { "cats" }, false, false) { Age = 4 };
+
+    // Default params don't need to be set. (Amount of stomachs)
+    Animal cow = new Cow("Bella", new[] { "Grass" });
+
+    /* Polymorphism and generics: A generic<T> list can store a list of any data type.
+        * But, once the data type is set, that list can only store objects of that type. E.g. A list of int cannot contain a string.
+        * Inheritance is a 'is - a ' relationship -> Cat is an animal, dog is an animal.
+        * Ergo -> These can be stored in a list of animals.
+     */
+    IList<Animal> animals = new List<Animal>();
+    animals.Add(cat);
+    animals.Add(dog);
+    animals.Add(cow);
+    return animals;
+}
+
+void PrintAnimals(IList<Animal> animals)
+{
+    foreach (Animal animal in animals)
+    {
+        // We can access all Properties of the Animal class in each of the animals.
+        Console.WriteLine($"Animal Id: {animal.Id}");
+        Console.WriteLine($"Name: {animal.Name}");
+        Console.WriteLine($"Age: {animal.Age}");
+
+        // Polymorphism and overriding
+        // Thanks to overriding, we can call the same method X amount of times and get a different result, based on the type of class being handled.
+        Console.WriteLine(animal.GetAnimalCall());
+
+        // BUT, we cannot access the extra Properties and Methods in the derived classes.
+        // To access these, we have to perform a conversion.
+        // Is and As are perfect for this as they check if the type can be converted first.
+        Carnivore carnivore = animal as Carnivore;
+        if (carnivore is not null)
+        {
+            // Single Responsability -> One methodod = One responsability
+            PrintCarnivoreInfo(carnivore);
+        }
+
+        Herbivore herbivore = animal as Herbivore;
+        if (herbivore is not null)
+        {
+            PrintHerbivoreInfo(herbivore);
+        }
+
+        Console.WriteLine("-------------------------------------");
+        Console.WriteLine();
+    }
+
+    Console.ReadLine();
+}
+
+void PrintCarnivoreInfo(Carnivore carnivore)
+{
+    Console.WriteLine($"This predator enjoys hunting: {string.Join(" and ", carnivore.FavouritePrey)}");
+    Console.WriteLine($"Is this predator lethal to humans?: {(carnivore.IsDangerousToHumans ? "Yes" : "No")}");
+
+    Cat cat = carnivore as Cat;
+    if (cat != null)
+    {
+        Console.WriteLine($"Is this kitty a house cat?: {(cat.IsHouseCat ? "Yes" : "No")}");
+    }
+
+    Dog dog = carnivore as Dog;
+    if (dog != null)
+    {
+        Console.WriteLine($"Is this dog a good boy?: {(dog.IsGoodBoy ? "Ofcourse" : "No, but actually yes")}");
+        dog.FetchStick();
+    }
+}
+
+void PrintHerbivoreInfo(Herbivore herbivore)
+{
+    Console.WriteLine($"This herbivore enjoys: {string.Join("and", herbivore.FavouriteFoods)}");
+
+    // Is returns a bool -> If true, this class can be derived into a deriving class
+    // As is usually more performant.
+    if (herbivore is Cow)
+    {
+        Cow cow = (Cow)herbivore;
+        Console.WriteLine($"This cow has {cow.AmountOfStomachs} stomachs");
+
+        cow.ProduceMilk();
+    }
+}
